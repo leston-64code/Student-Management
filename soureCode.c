2529c8952b9c694bf18f12fbb0b453d4a3253d2d
Leston <lestonair6574@gmail.com>
2023-08-29 07:38:56 +0530

    implemented student functions

--- soureCode.c
+++ soureCode.c
@@ -65,83 +65,92 @@ void addStudent(struct student **root) {
         *root = newstudent;
     }
     printf("Enter the student's name, class, rollno, gender(m/f): ");
-    scanf("%s%d%d%c",name,&class,&rollno,&gender);
+    scanf("%s %d %d %c", name, &class, &rollno, &gender);
     for (int i = 0; i < strlen(name); i++) {
-        name[i]=tolower(name[i]);
+        name[i] = tolower(name[i]);
     }
     strcpy(newstudent->name,name);
-    newstudent->class=class;
-    newstudent->rollno=rollno;
-    newstudent->gender=gender;
-    newstudent->avgscore=0.0;
-    newstudent->next=NULL;
+    newstudent->class = class;
+    newstudent->rollno = rollno;
+    newstudent->gender = gender;
+    newstudent->avgscore = 0.0;
+    newstudent->next = NULL;
 
     char choice;
     printf("Do you want to enter the student's marks now? (y/n): ");
     scanf(" %c", &choice);
 
-    if (choice=='y'||choice=='Y'){
-        newstudent->applied_subjects=(struct subjects*)malloc(sizeof(struct subjects));
+    if (choice == 'y' || choice == 'Y') {
+        newstudent->applied_subjects = (struct subjects*)malloc(sizeof(struct subjects));
         printf("Enter marks for Maths, Science, Social Studies, English, and Ethics:\n");
-        scanf("%d%d%d%d%d",&newstudent->applied_subjects->maths,&newstudent->applied_subjects->science,&newstudent->applied_subjects->social_studies,&newstudent->applied_subjects->english,&newstudent->applied_subjects->ethics);
+        scanf("%d %d %d %d %d",&newstudent->applied_subjects->maths,&newstudent->applied_subjects->science,&newstudent->applied_subjects->social_studies,&newstudent->applied_subjects->english,&newstudent->applied_subjects->ethics);
     } else {
         newstudent->applied_subjects = NULL;
     }
 }
 
 void updateStudent(struct student *root) {
-    int rollno;
-    printf("Enter the roll number of the student to update: ");
-    scanf("%d", &rollno);
+    if (isEmpty(root)) {
+        return;
+    }
 
-    struct student *presentNode = root;
-    while (presentNode != NULL) {
-        if (presentNode->rollno == rollno) {
+    int maths,science,social_studies,english,ethics;
+
+    int class,rollno;
+    printf("Enter the class and roll no of student you want to delete\n");
+    printf("Class :- ");
+    scanf("%d",&class);
+    printf("Rollno :- ");
+    scanf("%d",&rollno);
+
+    struct student *currentNode = root;
+    while (currentNode != NULL) {
+        if (currentNode->rollno == rollno) {
             printf("Enter the updated class and rollno: ");
-            scanf("%d %d", &presentNode->class, &presentNode->rollno);
+            scanf("%d %d", &currentNode->class, &currentNode->rollno);
 
             char response;
-            printf("Do you want to update subject marks for this student? (y/n): ");
+            printf("Do you want to update subject marks for this student?(y/n): ");
             scanf(" %c", &response);
 
             if (response == 'y' || response == 'Y') {
-                if (presentNode->applied_subjects == NULL) {
-                    presentNode->applied_subjects = (struct subjects*)malloc(sizeof(struct subjects));
+                if (currentNode->applied_subjects == NULL) {
+                    currentNode->applied_subjects = (struct subjects*)malloc(sizeof(struct subjects));
                 }
 
-                printf("Update Subjects:\n");
+                printf("Enter updated marks for Maths, Science, Social Studies, English, and Ethics:\n");
                 printf("Enter -1 for subjects you don't want to update.\n");
+                scanf("%d %d %d %d %d",&maths,&science,&social_studies,&english,&ethics);
 
-                if (presentNode->applied_subjects->maths != -1) {
-                    printf("Maths: ");
-                    scanf("%d", &presentNode->applied_subjects->maths);
-                }
-                if (presentNode->applied_subjects->science != -1) {
-                    printf("Science: ");
-                    scanf("%d", &presentNode->applied_subjects->science);
-                }
-                if (presentNode->applied_subjects->social_studies != -1) {
-                    printf("Social Studies: ");
-                    scanf("%d", &presentNode->applied_subjects->social_studies);
-                }
-                if (presentNode->applied_subjects->english != -1) {
-                    printf("English: ");
-                    scanf("%d", &presentNode->applied_subjects->english);
-                }
-                if (presentNode->applied_subjects->ethics != -1) {
-                    printf("Ethics: ");
-                    scanf("%d", &presentNode->applied_subjects->ethics);
+                if(maths!=-1){
+                    currentNode->applied_subjects->maths=maths;
                 }
 
-                printf("Subject marks updated successfully.\n");
+                if(science!=-1){
+                    currentNode->applied_subjects->science=science;
+                }
+
+                if(social_studies!=-1){
+                    currentNode->applied_subjects->social_studies=social_studies;
+                }
+
+                if(english!=-1){
+                    currentNode->applied_subjects->english=english;
+                }
+
+                if(ethics!=-1){
+                    currentNode->applied_subjects->ethics=ethics;
+                }
+
+                printf("Subject marks updated successfully..............\n");
             }
 
-            printf("Student details updated successfully.\n");
+            printf("Student details updated successfully..............\n");
             return;
         }
-        presentNode = presentNode->next;
+        currentNode = currentNode->next;
     }
-    printf("Student with roll number %d not found.\n", rollno);
+    printf("Student with roll number %d and class %d not found.\n",rollno,class);
 }
 
 void getAllStudents(struct student *root) {
@@ -161,13 +170,23 @@ void getStudentsOfAParitcularClass(struct student *root) {
     printf("Students of class %d:\n", selectedClass);
     
     struct student *presentNode = root;
+    printf("SL NO\t\tNAME\t\tCLASS\t\tROLL NO\t\tSCORE(%)\tGENDER\n");
+    int count=0; 
+    char nameString[20],upper;
     while (presentNode != NULL) {
         if (presentNode->class == selectedClass) {
-            printf("Name: %s, Class: %d, Rollno: %d\n",
-                   presentNode->name, presentNode->class, presentNode->rollno);
-        }
+            ++count;
+            strcpy(nameString,presentNode->name);
+            if(strlen(nameString)>=13){
+                nameString[13]='\0';
+            };
+            nameString[0]=toupper(nameString[0]);
+            upper=toupper(presentNode->gender);
+            printf("%d\t\t%s\t\t%d\t\t%d\t\t%.2f%\t\t%c\n",count,nameString,presentNode->class,presentNode->rollno,upper
+            );
+       }
         presentNode = presentNode->next;
-    }
+}
 }
 
 void calculateAverageScore(struct student *root){
@@ -218,13 +237,47 @@ void deleteAParticularStudent(struct student *root) {
     printf("Student with class %d and roll number %d deleted successfully.\n",class,rollno);
 }
 
+void getAParticularStudent(struct student *root) {
+    if (isEmpty(root)) {
+        return;
+    }
+
+    int class,rollno;
+    printf("Enter the class and roll no of student you want to delete\n");
+    printf("Class :- ");
+    scanf("%d",&class);
+    printf("Rollno :- ");
+    scanf("%d",&rollno);
+
+    struct student *presentNode = root;
+    printf("NAME\t\tCLASS\t\tROLL NO\t\tSCORE(%)\tGENDER\n");
+    char nameString[20],upper;
+
+    while (presentNode != NULL) {
+        if (presentNode->class==class&&presentNode->rollno==rollno) {
+            strcpy(nameString,presentNode->name);
+            if(strlen(nameString)>=13){
+                nameString[13]='\0';
+            };
+            nameString[0]=toupper(nameString[0]);
+            upper=toupper(presentNode->gender);
+            printf("%s\t\t%d\t\t%d\t\t%.2f%\t\t%c\n",nameString,presentNode->class,presentNode->rollno,upper
+            );
+        }
+        presentNode = presentNode->next;
+    }
+
+    printf("Student with class %d and roll number %d not found.\n",class,rollno);
+    return; 
+}
+
 
 int main() {
     int ch;
     struct student *root = NULL;
     while (1) {
         printf("Enter your choice\n");
-        printf("1. Add student\n2. Update student details\n3. Get all students\n4. Get students of a class\n5.Calculate Average Score\n");
+        printf("1. Add student\n2. Update student details\n3. Get all students\n4. Get students of a class\n5.Calculate Average Score\n6.Delete a particular student\n");
         scanf("%d", &ch);
         switch (ch) {
             case 1:
@@ -246,6 +299,9 @@ int main() {
                 deleteAParticularStudent(root);
                 break;
             case 7:
+                getAParticularStudent(root);
+                break;
+            case 8:
                 return 0;
             default:
                 printf("Invalid Choice\n");
